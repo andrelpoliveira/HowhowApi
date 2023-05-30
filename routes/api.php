87e44c461bcbf6adbe600a82bdf0cc2b27425733
Lownzito <luisotaviown@gmail.com>
2023-05-30 15:39:58 -0300

    rota para puxada de generos

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/aaaaaaa', [CampaignController::class , 'tratamentojson']);
 
 //não tem login, aceita login de todos os tipos de usuarios
 Route::post('/login', [LoginController::class, 'login']);
