2998d91de6b819e88f20855b7cb0024062bb1f74
Lownzito <luisotaviown@gmail.com>
2023-05-31 15:30:32 -0300

    oq andre pediu

--- app/Http/Resources/Campaign/CampaignListResource.php
+++ app/Http/Resources/Campaign/CampaignListResource.php
@@ -20,13 +20,13 @@ class CampaignListResource extends JsonResource
             'brand_info'        => $this->brand_info,    
             'campaign_purpose'  => $this->campaign_purpose,
             'country'           => $this->country,
-            'states'            => $this->states,
+            'states'            => json_decode($this->states),
             'line_of_business'  => $this->line_of_business,
             'campaign_photo'    => $this->campaign_photo,
             'private'           => $this->private,
             'ended'             => $this->ended,
-            'social_media'      => $this->social_media,
-            'content_type'      => $this->content_type,
+            'social_media'      => json_decode($this->social_media),
+            'content_type'      => json_decode($this->content_type),
         ];
     }
 }
