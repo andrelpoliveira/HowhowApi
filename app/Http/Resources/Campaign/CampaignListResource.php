ebc813ae7d7bca7066d2164f46838adcfb328f82
andrelpoliveira <andreluiz.pradodeoliveira@gmail.com>
2023-05-31 15:22:40 -0300

    CampaignListresource

--- app/Http/Resources/Campaign/CampaignListResource.php
+++ app/Http/Resources/Campaign/CampaignListResource.php
@@ -17,12 +17,16 @@ class CampaignListResource extends JsonResource
         return [
             'name'              => $this->name,
             'brand_name'        => $this->brand_name,
-            'budget'            => $this->budget,
-            'category'          => $this->category,
+            'brand_info'        => $this->brand_info,    
+            'campaign_purpose'  => $this->campaign_purpose,
+            'country'           => $this->country,
+            'states'            => $this->states,
             'line_of_business'  => $this->line_of_business,
             'campaign_photo'    => $this->campaign_photo,
             'private'           => $this->private,
             'ended'             => $this->ended,
+            'social_media'      => $this->social_media,
+            'content_type'      => $this->content_type,
         ];
     }
 }
